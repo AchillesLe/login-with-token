9ee2b98d6e03f7cb1d8727dbe244b8e2bd083988
DESKTOP-AQQD4TP\User <lvbao.cntt.sgu@gmail.com>
2018-07-09 10:55:05 +0700

    no commit

---
+++ login-with-token/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { SharedComponent } from './shared/shared.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent,
+    SharedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
